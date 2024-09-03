ceb72baab5f00b919f3d366de562940a2b60a9ba
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2024-09-03 14:49:59 -0600

    Added LottoRandom

--- src/LottoInput.js
+++ src/LottoInput.js
@@ -1,17 +1,25 @@
 export default function LottoInput({ value, onChange, player, onPlayerChange, onClick }) {
     return (
         <>
+        <div style={{display:'flex', flexDirection:'column', alignItems:'center', gap:'10px'}}>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '10px', marginBlock:'0px' }}>
+            <div>Player:</div>
             <input
                 type="player"
                 value={player}
                 onChange={onPlayerChange}
             />
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                <div>Lotto Number: </div>
             <input
                 type="number"
                 value={value}
                 onChange={onChange}
-            />
+                />
+            </div>
             <button onClick={onClick}>Add Ticket</button>
+            </div>
         </>
     );
 }
